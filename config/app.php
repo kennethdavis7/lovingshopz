c3f0ff4e0353dab2b6bdee8a59ab53246e65d192
Kenneth Davis <kennethdaviskd7@gmail.com>
2026-06-24 15:43:06 +1000

    Remove debugbar from production providers

--- config/app.php
+++ config/app.php
@@ -163,7 +163,6 @@ return [
         /*
          * Application Service Providers...
          */
-        // Barryvdh\Debugbar\ServiceProvider::class,
         Barryvdh\Snappy\ServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
@@ -185,7 +184,6 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
-        // 'Debugbar' => Barryvdh\Debugbar\Facades\Debugbar::class,
     ])->toArray(),
 
 ];
